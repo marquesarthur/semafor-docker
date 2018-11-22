bd6490137bf459eae6afa7ff8b9577b7ed59caa9
Arthur Marques <marques.s.arthur@gmail.com>
2018-11-21 22:47:41 -0800

    Added docker database for later dump and port between mackos

--- semafor-rest/semviz/settings.py
+++ semafor-rest/semviz/settings.py
@@ -2,19 +2,20 @@
 Author: Sam Thomson (sthomson@cs.cmu.edu)
         Lingpeng Kong (lingpenk@cs.cmu.edu)
 """
+import os
 
 # SEMAFOR server settings
-SEMAFOR_HOST = "localhost"
+SEMAFOR_HOST = os.environ.get('DOCKER_HOST')
 SEMAFOR_PORT = 8085
 
 # Location of MaltParser and POS tagger executables
 SEMAFOR_HOME = "/semafor-master"
 
 # MST Parser server settings
-MST_HOST = "localhost"
+MST_HOST = os.environ.get('DOCKER_HOST')
 MST_PORT = 12345
 
 # caching parsed sentences in key-value NoSQL database
 CACHE_RESULTS = True
-MONGO_HOST = "mongodb://localhost"
+MONGO_HOST = "mongodb://{}".format(os.environ.get('DOCKER_HOST'))
 MONGO_PORT = 27017
